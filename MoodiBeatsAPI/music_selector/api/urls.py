f2bce1fbddef0dd3b12963e0e45aec100d7948bd
sebastian <jsh7590cr@gmail.com>
2020-01-21 01:55:16 -0500

    Se realizó en el Backend-view las funciones necesarias para filtrar daros por nombre, emocion, genero y se cambió la url de destino po runa solo con v2

--- MoodiBeatsAPI/music_selector/api/urls.py
+++ MoodiBeatsAPI/music_selector/api/urls.py
@@ -1,9 +1,10 @@
 # api/urls.py
 from django.conf.urls import url
-from .views import *
+from .views import NewVideoAPIView, getMoodData, getMoodGenreData, getNameData
 
 urlpatterns = [
  url(r'^new-videos/$', NewVideoAPIView.as_view()),
- url(r'^songs-moods/(?P<mood>\d{1})/$', getMoodData),
- url(r'^songs-genres-moods/(?P<mood>\d{1})/(?P<genre>\w+)/$', getMoodGenreData),
+ url(r'^songs/(?P<mood>\d{1})/$', getMoodData),
+ url(r'^songs/(?P<mood>\d{1})/(?P<genre>\w+)/$', getMoodGenreData),
+ url(r'^songs/(?P<name>\w+)/$', getMoodGenreData),
 ]
--- MoodiBeatsAPI/music_selector/api/views.py
+++ MoodiBeatsAPI/music_selector/api/views.py
@@ -7,26 +7,72 @@ from rest_framework.parsers import JSONParser
 from rest_framework import viewsets
 from django.http import JsonResponse
 
-# Create your views here.
-
-
 class NewVideoAPIView(generics.ListCreateAPIView):
+    #Clase que se encraga de retornar todos lo elementos por medio del serializcer
     queryset = NewVideo.objects.all()
     serializer_class = NewVideoSerializer
 
 def switch(argument):
+    #función que se encraga de pasar un argumento numérico a la evaluaciom del mims
     switcher = {1: 'HAPPY', 2: 'IN-LOVE', 3: 'SAD', 4: 'ANGRY'}
     return switcher.get(int(argument))
 
 def getMoodData(*args, **kwargs):
+    '''La función se encarga de realizar una consulya a la DB y extraer
+    elementos que cumplan com una emocion determinada
+
+    Args:
+        *args: Es una tupla de parámetros posicionales
+        **kwargs: Es un diccionario de parametros con nombre
+        
+    Returns:
+        Json:Con los valore ya filtrados.
+    '''
     numberMood = switch(kwargs.get('mood'))
     queryset = NewVideo.objects.filter(moods=numberMood)
-    data = list(queryset.values("video_id", "video_title"))
+    data = list(queryset.values("video_id", "video_title", "moods", "predicted_moods"))
     return JsonResponse(data, safe=False)
 
 def getMoodGenreData(*args, **kwargs):
+    '''La función se encarga de realizar una consulya a la DB y extraer
+    elementos que cumplan com una emocion y género determinado.
+
+    Args:
+        *args: Es una tupla de parámetros posicionales
+        **kwargs: Es un diccionario de parametros con nombre
+        
+    Returns:
+        Json:Con los valore ya filtrados.
+    '''
     numberMood = switch(kwargs.get('mood'))
     genero = kwargs.get('genre')
     queryset = NewVideo.objects.filter(moods=numberMood).filter(genre=genero)
-    data = list(queryset.values("video_id", "video_title"))
+    data = list(queryset.values('video_id', 'video_title', 'moods', 'predicted_moods'))
     return JsonResponse(data, safe=False)
+
+def getNameData(*args, **kwargs):
+    '''La función se encarga de realizar una consulya a la DB y extraer
+    elementos que cumplan con el nombre que se le pasa po parámetro.
+
+    Args:
+        *args: Es una tupla de parámetros posicionales
+        **kwargs: Es un diccionario de parametros con nombre
+        
+    Returns:
+        Json:Con los valore ya filtrados.
+    '''
+    name = switch(kwargs.get('name'))
+    queryset = NewVideo.objects.filter(video_title=name)
+    data = list(queryset.values('video_id', 'video_tittle', 'moods', 'predictes_moods'))
+    return JsonResponse(data, sage=False)
+
+    '''La función se encarga de realizar una consulya a la DB y extraer
+    elementos que cumplan com una emocion y género determinado.
+
+    Args:
+        *args: Es una tupla de parámetros posicionales
+        **kwargs: Es un diccionario de parametros con nombre
+
+    Returns:
+        Json:Con los valore ya filtrados.
+    '''
